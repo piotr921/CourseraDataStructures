e6f7d53a0229dd3a945b9989e702dd947db06461
piotr921 <piotr92345@o2.pl>
2017-10-30 15:22:49 +0100

    Start solving exercise 2.

---
+++ src/main/java/datastructures/week1/TreeHeight.java
@@ -0,0 +1,9 @@
+package datastructures.week1;
+
+/**
+ * @author Piotr_Sekula
+ * @since 30.10.2017.
+ */
+public class TreeHeight {
+
+}
---
+++ src/test/java/datastructures/week1/TreeHeightTest.java
@@ -0,0 +1,16 @@
+package datastructures.week1;
+
+import static org.testng.Assert.*;
+
+import org.testng.annotations.DataProvider;
+
+/**
+ * @author Piotr_Sekula
+ * @since 30.10.2017.
+ */
+public class TreeHeightTest {
+
+    @DataProvider
+    private 
+
+}
