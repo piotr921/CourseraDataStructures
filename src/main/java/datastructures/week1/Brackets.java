456cb6b10c7affdea376bd5cb1ac971aee5524fb
piotr921 <piotr92345@o2.pl>
2017-10-30 10:10:48 +0100

    Start exercises for week 1.

--- src/main/java/datastructures/week1/Brackets.java
+++ src/main/java/datastructures/week1/Brackets.java
@@ -1,4 +1,58 @@
 package datastructures.week1;
 
 public class Brackets {
+
+    public String check(String text) {
+        return "";
+    }
+
+    enum Type {
+        CIRCLE {
+            @Override
+            boolean isOpenChar(char c) {
+                return c == '(';
+            }
+
+            @Override
+            boolean isClosedChar(char c) {
+                return c == ')';
+            }
+        },
+        SQUARE {
+            @Override
+            boolean isOpenChar(char c) {
+                return c == '[';
+            }
+
+            @Override
+            boolean isClosedChar(char c) {
+                return c == ']';
+            }
+        },
+        CURLY {
+            @Override
+            boolean isOpenChar(char c) {
+                return c == '{';
+            }
+
+            @Override
+            boolean isClosedChar(char c) {
+                return c == '}';
+            }
+        };
+
+        abstract boolean isOpenChar(char c);
+        abstract boolean isClosedChar(char c);
+        Type selectType(char c) {
+            switch (c){
+                case '(':
+                    return CURLY;
+                    break;
+                case ')':
+                    return CURLY;
+                    break;
+
+            }
+        }
+    }
 }
--- src/test/java/datastructures/week1/BracketsTest.java
+++ src/test/java/datastructures/week1/BracketsTest.java
@@ -1,7 +1,28 @@
 package datastructures.week1;
 
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
 import static org.testng.Assert.*;
 
 public class BracketsTest {
 
+    @DataProvider
+    private Object[][] textProvider() {
+            return new Object[][]{
+                    {"[]", "Success"}
+        };
+    }
+
+    @Test(dataProvider = "textProvider")
+    public void shouldCheckIfBracketsMatch(String text, String expected) {
+        // Given
+        Brackets brackets = new Brackets();
+
+        // When
+        String result = brackets.check(text);
+
+        // Then
+        assertEquals(result, expected);
+    }
 }
